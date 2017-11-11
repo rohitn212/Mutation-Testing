34ad31c30d213d9bd1cc3cba039e86469ce14ad9
Harsh <hpate34@uic.edu>
2017-11-10 19:38:06 -0600

    added javassist dependancy

--- hw3/src/main/uk/ac/york/minesweeper/Launcher.java
+++ hw3/src/main/uk/ac/york/minesweeper/Launcher.java
@@ -3,13 +3,14 @@ package uk.ac.york.minesweeper;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Launcher {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         readConfig("configFile.txt");
     }
 
-    private static void readConfig(String filename) throws FileNotFoundException {
+    private static void readConfig(String filename) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(filename));
     }
 }
