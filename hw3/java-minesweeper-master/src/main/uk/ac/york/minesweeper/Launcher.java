fd17acefcfa292794d198e76780ae68a89f41807
Harsh <hpate34@uic.edu>
2017-11-10 19:08:11 -0600

    added intellij configs

--- hw3/java-minesweeper-master/src/main/uk/ac/york/minesweeper/Launcher.java
+++ hw3/java-minesweeper-master/src/main/uk/ac/york/minesweeper/Launcher.java
@@ -2,6 +2,10 @@ package uk.ac.york.minesweeper;
 
 public class Launcher {
     public static void main(String[] args) {
-        
+        readConfig("");
+    }
+
+    private static void readConfig(String filename) {
+
     }
 }
